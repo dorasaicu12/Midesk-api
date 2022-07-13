9f3c07b6043c7036127b48470cb752fc42eca533
hoai anh <xicoi1033@gmail.com>
2022-07-13 14:43:40 +0700

    chinh sua role va bo phan hien ra trong login

--- app/Models/TeamStaff.php
+++ app/Models/TeamStaff.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Team;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,29 @@ class TeamStaff extends Model
 	public function Team()
 	{
 		return $this->hasOne(Team::class,'team_id','team_id');
+	}
+
+	public function getTeamInfor($id){
+		$team=self::where('agent_id',$id)->get();
+		$team_id= '';
+		$team_infor=array();
+		 if($team != ''){
+			foreach($team as $k =>$val){
+			   $team_id=  $val['team_id'];
+				if($team_id==''){
+					$team_infor=[];
+				 }else{
+					$team_depart=Team::where('team_id',$team_id)->get();
+					foreach($team_depart as $k2 => $val2){
+						$team_infor[]=$val2['team_name'];
+					}
+				 }
+			 }
+
+		}else{
+			$team_infor=[];
+		}
+
+	return $team_infor;
 	}
 }
