47ccf6e107fcd97403d64205436c3e56f642fc03
hoai anh <xicoi1033@gmail.com>
2022-07-14 08:38:19 +0700

    appservice conflict

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,7 +27,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     { 
         // \URL::forceScheme("http");
-        // \URL::forceScheme("https");
+        if (App::environment() === "production") {
+            //file .env: APP_ENV=production
+            \URL::forceScheme("https");
+        }
+        
         Collection::macro('paginate', function ($perPage, $total = null, $page = null, $pageName = 'page') {
             $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
             return new LengthAwarePaginator($this->forPage($page, $perPage), $total ?: $this->count(), $perPage, $page, [
